4322622c064531a9f2d9e5dcfd8052532d1b51ed
ExodiaViolet <unbyy62@gmail.com>
2025-04-30 20:44:18 +0100

    Model run

--- deep/modelling/metric_utils.py
+++ deep/modelling/metric_utils.py
@@ -37,11 +37,6 @@ def get_fitted_model_metrics(model):
     best_val_pre = history['val_precision'][best_epoch]
     print(f"Train Precision:{best_train_pre}\nValidation Precision: {best_val_pre}")
 
-    # Alternatively, to get precision per class
-    if 'precision' in history and isinstance(history['precision'], list):
-        for i, class_precision in enumerate(history['precision'][best_epoch]):
-            print(f"Class {i} Precision: {class_precision}")
-
     return best_val_pre
 
 def plot_metrics(model, binary=False):
